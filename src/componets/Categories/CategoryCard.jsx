ccf574e929608448f1440da4d7cb48719e0011b3
Vodnala Santhosh <santhoshvodnala42@gmail.com>
2025-09-10 23:35:03 +0530

    change done

--- src/componets/Categories/CategoryCard.jsx
+++ src/componets/Categories/CategoryCard.jsx
@@ -3,7 +3,7 @@ const CategoryCard = ({ category }) => {
     <div className="bg-white p-6 rounded-2xl shadow hover:shadow-lg transition flex flex-col items-center text-center">
       <div className="text-3xl mb-4">{category.icon}</div>
       <h3 className="text-lg font-semibold">{category.name}</h3>
-      <p className="text-gray-600">{category.jobs} Jobs</p>
+      <p className="text-gray-600  ">{category.jobs} Jobs</p>
     </div>
   );
 };
